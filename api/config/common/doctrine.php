abc054150488d35172fe5fa769127e20c312d3a9
Andrej Pedchenko <graduadu@gmail.com>
2021-07-15 17:03:05 +0300

    Added alternative configuration

--- api/config/common/doctrine.php
+++ api/config/common/doctrine.php
@@ -48,9 +48,12 @@ return [
             'cache_dir'     => __DIR__ . '/../../var/cache/doctrine/cache',
             'proxy_dir'     => __DIR__ . '/../../var/cache/doctrine/proxy',
             'connection'    => [
-                'driver'  => 'pdo_pgsql',
-                'url'     => getenv('DB_URL'),
-                'charset' => 'utf-8'
+                'driver'   => 'pdo_pgsql',
+                'host'     => getenv('DB_HOST'),
+                'user'     => getenv('DB_USER'),
+                'password' => getenv('DB_PASSWORD'),
+                'dbname'   => getenv('DB_NAME'),
+                'charset'  => 'utf-8'
             ],
             'metadata_dirs' => [],
         ],
