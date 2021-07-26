5d12e141bc1e3889d6848eab60ff14d715b69ebe
Andrej Pedchenko <graduadu@gmail.com>
2021-07-26 08:59:21 +0300

    Added database console commands

--- api/bin/app.php
+++ api/bin/app.php
@@ -3,6 +3,9 @@
 
 declare(strict_types=1);
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
@@ -19,6 +22,13 @@ $cli = new Application('Console');
  * @psalm-suppress MixedArrayAccess
  */
 $commands = $container->get('config')['console']['commands'];
+
+/** @var EntityManagerInterface $entityManager*/
+$entityManager = $container->get(EntityManagerInterface::class);
+$cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+
+ConsoleRunner::addCommands($cli);
+
 foreach ($commands as $name) {
   /** @var Command $command */
     $command = $container->get($name);
