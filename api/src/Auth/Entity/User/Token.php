1d4802c14e66be83e414e7fa1882a53152211715
Andrej Pedchenko <graduadu@gmail.com>
2021-07-30 14:29:47 +0300

    Added token embedded mapping

--- api/src/Auth/Entity/User/Token.php
+++ api/src/Auth/Entity/User/Token.php
@@ -5,19 +5,31 @@ declare(strict_types=1);
 namespace App\Auth\Entity\User;
 
 use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
 use DomainException;
 use Webmozart\Assert\Assert;
 
+/**
+ * @ORM\Embeddable
+ */
 class Token
 {
-    private string $value;
-    private DateTimeImmutable $expires;
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $value;
+
+    /**
+     * @var DateTimeImmutable
+     * @ORM\Column(type="datetime_immutable", nullable=true)
+     */
+    private $expires;
 
     /**
      * Token constructor.
      * @param string            $value
      * @param DateTimeImmutable $expires
-     * @noinspection PhpFieldAssignmentTypeMismatchInspection
      */
     public function __construct(string $value, DateTimeImmutable $expires)
     {
