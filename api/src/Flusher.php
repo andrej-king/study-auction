5eeb7fbf03fc0acadb3b465e732e8f4dcf3bd41e
Andrej Pedchenko <graduadu@gmail.com>
2021-08-04 16:36:23 +0300

    Implemented flusher

--- api/src/Flusher.php
+++ api/src/Flusher.php
@@ -4,10 +4,25 @@ declare(strict_types=1);
 
 namespace App;
 
-interface Flusher
+use Doctrine\ORM\EntityManagerInterface;
+
+class Flusher
 {
+    private EntityManagerInterface $em;
+
+    /**
+     * Flusher constructor.
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * Save data in db
      */
-    public function flush(): void;
+    public function flush(): void
+    {
+        $this->em->flush();
+    }
 }
