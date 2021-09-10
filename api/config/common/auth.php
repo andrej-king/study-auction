f505ce8c308b0bdf69aa635133f497294443369e
Andrew <graduadu@gmail.com>
2021-09-10 15:14:50 +0300

    Added tokenizer factory

--- api/config/common/auth.php
+++ api/config/common/auth.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Auth\Entity\User\User;
 use App\Auth\Entity\User\UserRepository;
+use App\Auth\Service\Tokenizer;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Psr\Container\ContainerInterface;
@@ -16,4 +17,20 @@ return [
         $repo = $em->getRepository(User::class);
         return new UserRepository($em, $repo);
     },
+
+    Tokenizer::class => function (ContainerInterface $container): Tokenizer {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @psalm-var array{token_ttl:string} $config
+         */
+        $config = $container->get('config')['auth'];
+
+        return new Tokenizer(new DateInterval($config['token_ttl']));
+    },
+
+    'config' => [
+        'auth' => [
+            'token_ttl' => 'PT1H', // token alive time 1 hour
+        ],
+    ],
 ];
