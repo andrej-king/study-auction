8a1192524b8d92cd0ee0acc5894f5ee41c84e730
Andrey <graduadu@gmail.com>
2021-09-27 15:58:09 +0300

    Installed symfony validator.

---
+++ api/config/common/validator.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+// register ValidatorInterface
+return [
+    ValidatorInterface::class => function (): ValidatorInterface {
+        /** @psalm-suppress DeprecatedMethod */
+        AnnotationRegistry::registerLoader('class_exists');
+
+        return Validation::createValidatorBuilder()
+            ->enableAnnotationMapping()
+            ->getValidator();
+    }
+];
