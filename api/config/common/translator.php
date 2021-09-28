e5b1e38466eff2ad5b1b6b5964911677260eb2c1
Andrey <graduadu@gmail.com>
2021-09-28 10:09:55 +0300

    Added validation translations.

--- api/config/common/translator.php
+++ api/config/common/translator.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 // register Translator for languages
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Translation\Loader\PhpFileLoader;
+use Symfony\Component\Translation\Loader\XliffFileLoader;
 use Symfony\Component\Translation\Translator;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -11,16 +13,32 @@ return [
     TranslatorInterface::class => function (ContainerInterface $container): TranslatorInterface {
         /**
          * @psalm-suppress MixedArrayAccess
-         * @psalm-var array{'lang':'string','resources':'array<string[]>'} $config
+         * @psalm-var array{lang:string,resources:array<string[]>} $config
          */
         $config = $container->get('config')['translator'];
 
-        return new Translator($config['lang']);
+        $translator = new Translator($config['lang']);
+        $translator->addLoader('php', new PhpFileLoader());
+        $translator->addLoader('xlf', new XliffFileLoader());
+
+        foreach ($config['resources'] as $resource) {
+            $translator->addResource(...$resource);
+        }
+
+        return $translator;
     },
 
     'config' => [
         'translator' => [
             'lang' => 'en',
+            'resources' => [
+                [
+                    'xlf',
+                    __DIR__ . '/../../vendor/symfony/validator/Resources/translations/validators.ru.xlf',
+                    'ru',
+                    'validators'
+                ],
+            ],
         ],
     ],
 ];
