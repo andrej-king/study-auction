ba0674e4ba44a0feccd6de1519a26d9e766d16ec
Andrey <graduadu@gmail.com>
2021-09-28 10:13:16 +0300

    Injected validator translator.

--- api/config/common/validator.php
+++ api/config/common/validator.php
@@ -3,17 +3,24 @@
 declare(strict_types=1);
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 // register ValidatorInterface
 return [
-    ValidatorInterface::class => function (): ValidatorInterface {
+    ValidatorInterface::class => function (ContainerInterface $container): ValidatorInterface {
         /** @psalm-suppress DeprecatedMethod */
         AnnotationRegistry::registerLoader('class_exists');
 
+        /** @var TranslatorInterface $translator */
+        $translator = $container->get(TranslatorInterface::class);
+
         return Validation::createValidatorBuilder()
             ->enableAnnotationMapping()
+            ->setTranslator($translator)
+            ->setTranslationDomain('validators')
             ->getValidator();
     }
 ];
