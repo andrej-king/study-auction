30d46529ec2b8a38725cdbb71a0ea5465c087450
Andrew <graduadu@gmail.com>
2021-10-16 15:23:39 +0300

    Added import jest-dom/extend-expect from testing-library

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 import App from './App';
 
 test('renders app', () => {
-  const { getByText } = render(<App />);
+  const {getByText} = render(<App/>);
   const h1Element = getByText(/Auction/i);
   expect(h1Element).toBeInTheDocument();
 });
--- frontend/src/components/Welcome.test.jsx
+++ frontend/src/components/Welcome.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 import Welcome from "./Welcome";
 
 test('renders welcome', () => {
-  const { getByText } = render(<Welcome />);
+  const {getByText} = render(<Welcome/>);
   const h1Element = getByText(/Auction/i);
   expect(h1Element).toBeInTheDocument();
 });
