238005235931fad2991aeb0c3f4a6890d78215e8
Andrej Pedchenko <graduadu@gmail.com>
2021-11-21 11:21:53 +0200

    Implemented home welcome checking. Added data-testid attribute.

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import styles from './Welcome.module.css'
 
 const Welcome = () => {
   return (
-    <div className={styles.welcome}>
+    <div data-testid="welcome" className={styles.welcome}>
       <h1>Auction</h1>
       <p>We will be here soon</p>
     </div>
